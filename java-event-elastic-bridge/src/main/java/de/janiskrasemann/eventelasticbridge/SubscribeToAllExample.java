8f0e61718fda161a8fa9c85ce043c488daeb3c99
Janis Krasemann <janis.krasemann@enpit.de>
2018-01-12 00:54:44 +0100

    WIP posting the event data to elastic

--- java-event-elastic-bridge/src/main/java/de/janiskrasemann/eventelasticbridge/SubscribeToAllExample.java
+++ java-event-elastic-bridge/src/main/java/de/janiskrasemann/eventelasticbridge/SubscribeToAllExample.java
@@ -9,8 +9,14 @@ import eventstore.j.EsConnection;
 import eventstore.j.EsConnectionFactory;
 import eventstore.j.SettingsBuilder;
 
+import java.io.BufferedReader;
 import java.io.Closeable;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 public class SubscribeToAllExample {
@@ -37,6 +43,11 @@ public class SubscribeToAllExample {
             byte[] bytes = content.value().toArray();
             String eventData = new String(bytes, StandardCharsets.UTF_8);
             system.log().info(eventData);
+            try {
+              post("http://localhost:9200/ui-data/data/?pretty", eventData); 
+            } catch (IOException e) {
+              System.err.println(e.toString());
+            }
           }
 
           @Override
@@ -50,5 +61,41 @@ public class SubscribeToAllExample {
           }
         };
         final Closeable closeable = connection.subscribeToStream(streamName, subscription, false, null);
+    }
+
+    private static void post(String url, String data) throws IOException {
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+       
+        // Setting basic post request
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type","application/json");
+       
+        String postJsonData = data;
+        
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(postJsonData);
+        wr.flush();
+        wr.close();
+       
+        int responseCode = con.getResponseCode();
+        System.out.println("nSending 'POST' request to URL : " + url);
+        System.out.println("Post Data : " + postJsonData);
+        System.out.println("Response Code : " + responseCode);
+       
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String output;
+        StringBuffer response = new StringBuffer();
+       
+        while ((output = in.readLine()) != null) {
+         response.append(output);
+        }
+        in.close();
+        
+        //printing result from response
+        System.out.println(response.toString());
     }
 }
