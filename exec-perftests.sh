b9c2af284910fce641a9a52cb20bb23732b3bf2a
Janis Krasemann <janiskra@mail.uni-paderborn.de>
2018-03-21 19:10:04 +0100

    Add script for executing the perf tests

---
+++ exec-perftests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+for i in 100 1000 10000 100000
+do
+  size=$i
+  echo "$size"
+  for (( j=0; j<=7; j++ ))
+  do
+    factor=$((2^$j))
+    client_buffer=$(($factor*40))
+    buff_size=$(($factor*500))
+    live_buff_size=$(($factor*500))
+    read_batch=$(($factor*20))
+    echo "Performance test for size=$size, client buffer=$client_buffer, buffer size=$buff_size, live buffer size=$live_buff_size, read batch=$read_batch"
+  #   source ./performance.sh $size $client_buffer $buff_size $live_buff_size $read_batch
+  done
+done
