881e9fdead8974004f6b47d47eda348f54e8a57d
Janis Krasemann <janiskra@mail.uni-paderborn.de>
2018-03-22 02:15:17 +0100

    Update scripts for analysis & multiple runs

--- exec-perftests.sh
+++ exec-perftests.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-for i in 100 1000 10000 100000
+runs=$1
+for (( run=0; run<$runs; run++ ))
 do
-  size=$i
-  echo "$size"
-  for (( j=0; j<=9; j++ ))
+  mkdir "perf-test-$run"
+  for (( i=0; i<=3; i++ ))
   do
-    factor=$((2**$j))
-    client_buffer=$(($factor*40))
-    buff_size=$(($factor*500))
-    live_buff_size=$(($factor*500))
-    read_batch=$(($factor*20))
-    echo "Performance test for size=$size, client buffer=$client_buffer, buffer size=$buff_size, live buffer size=$live_buff_size, read batch=$read_batch"
-    source ./performance.sh $size $client_buffer $buff_size $live_buff_size $read_batch
+    size=$((10**($i+2)))
+    for (( j=0; j<=9; j++ ))
+    do
+      factor=$((2**$j))
+      number=$((10+$i*10+$j))
+      client_buffer=$(($factor*40))
+      buff_size=$(($factor*500))
+      live_buff_size=$(($factor*500))
+      read_batch=$(($factor*20))
+      echo "Experiment number $number of run $run, size $size"
+      # echo "Performance test no. $number for size=$size, client buffer=$client_buffer, buffer size=$buff_size, live buffer size=$live_buff_size, read batch=$read_batch"
+      source ./performance.sh $size $client_buffer $buff_size $live_buff_size $read_batch $number $run
+    done
   done
 done
--- performance.sh
+++ performance.sh
@@ -9,6 +9,8 @@ if [ -z ${3+x} ]; then BUFF_SIZE=$2; else BUFF_SIZE=500; fi
 if [ -z ${4+x} ]; then LIVE_BUFF_SIZE=$3; else LIVE_BUFF_SIZE=500; fi
 if [ -z ${5+x} ]; then READ_BATCH=$5; else READ_BATCH=20; fi
 
+touch perf-test-$7/performance-$6.csv
+
 docker-compose run -e EXPERIMENT_SIZE=$1 \
     -e BUFFER_SIZE=$3 \
     -e LIVE_BUFFER_SIZE=$4 \
@@ -21,10 +23,8 @@ docker-compose run -e STREAM=performance-$1\
     -e BUFFER_SIZE=$2 \
     --name evt-es-bridge-$1 \
     --rm evt-es-bridge \
-> perf-test/performance-$1-$3-$4-$5-$2.txt
+> perf-test-$7/performance-$6.csv
 
 echo "running performance test with $1 events, client-side buffersize $2, server-side buffersize $3, live buffer size $4, read batch size $5"
-echo "writing performance test results to performance-$1-$3-$4-$5-$2.txt"
-
-cat perf-test/performance-$1-$3-$4-$5-$2.txt
+cat perf-test-$7/performance-$6.csv
 
