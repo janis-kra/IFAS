c58e96063be480d3fb6253959d77d8e88f342332
Janis Krasemann <janiskra@mail.uni-paderborn.de>
2018-03-25 20:39:11 +0200

    Update for new experiment sizes

--- analyze-performance-tests.R
+++ analyze-performance-tests.R
@@ -3,9 +3,9 @@ library(ggplot2)
 setwd("/Users/jk/Documents/Uni/master/impl/eek")
 
 # constants etc.
-sizes <- 4
+sizes <- 3
 steps <- 10
-runs <- 3
+runs <- 1 # at least 1 run - overwritten later
 
 # functions
 
@@ -25,7 +25,7 @@ readFile <- function (file, folder) {
   runNumber <- getRunNumber(folder)
   configNumber <- expNumber %% steps # zero based!
   sizeNumber <- floor(expNumber / steps)
-  size <- 10^(sizeNumber+2)
+  size <- (sizeNumber+1) * 50000
   path <- paste(folder, file, sep = "/")
   data <- read.csv(path, header = TRUE, sep = ";", quote = "\"", dec = ".", fill = TRUE, comment.char = "")
   names(data) <- c("amount", "totalAmount", "timestamp", "comment")
@@ -47,50 +47,43 @@ results <- lapply(results.folders, function (folder) {
     readFile(file, folder)
   })
 })
+runs <- length(results)
 results.unlisted <- unlist(results, recursive = FALSE)
 results.flat <- do.call("rbind", results.unlisted)
 results.durations <- do.call("rbind", lapply(results.unlisted, function (df) {
   tail(df, 1)
 }))
 
-results.firstRun <- results[[1]]
-
-e100k.list <- results.firstRun[c(31:40)]
-e_100 <- do.call(rbind, results.firstRun[c(1:10)])
-e_1000 <- do.call(rbind, results.firstRun[c(11:20)])
-e_10000 <- do.call(rbind, results.firstRun[c(21:30)])
-e100k.data <- do.call(rbind, e100k.list)
-e_200000 <- do.call(rbind, results.firstRun[c(31:40)])
-
-plotAmount <- function (d) {
+plotAmount <- function (d, x.limit, x.intervals) {
   ggplot(d, aes(x = d$t, y = d$amount)) +
-    geom_line() +
+    #scale_y_log10() + # log scale not good because fluctuations at < 1000 events are not interesting
     facet_wrap(~config, ncol=2) +
-    geom_smooth(method = 'loess', color = 'red', se = FALSE, span = 0.4)
-  # todo: use log scale? maybe not?
-  # todo: use which ncol value for face_wrap?
-  # todo: legend
+    scale_x_continuous(name="Time (s)", breaks = seq(0, 100, x.intervals), limits = c(0, x.limit)) +
+    scale_y_continuous(name = "Events") +
+    geom_smooth(method = 'loess', color = 'red', se = FALSE, span = 0.4) +
+    geom_line()
+  # todo: use which ncol value for face_wrap? 2 seems good
 }
 
 plotTotalDuration <- function (d) {
-  ggplot(d, aes(x=config, y=t, group = config)) + geom_boxplot()
-  # todo legend
+  ggplot(d, aes(x=config, y=t, group = config)) +
+    geom_boxplot() +
+    scale_x_continuous(name = "Configuration", breaks = c(0:10)) +
+    scale_y_log10(name = "Duration (s)", breaks = seq(0, 100, 10))
+  # todo konfidenzintervalle anpassen?
+  # todo mehr experimente
 }
 
 results.run0 <- subset(results.flat, run == 0)
-plotAmount(subset(results.run0, size == 100000))
+
+plotAmount(subset(results.run0, size == 100000), x.limit = 100, x.intervals = 10)
 ggsave("config-comparison_100k.pdf", device = "pdf")
 
-# durations <- vapply(results.flat, function (df) {
-#   tail(df,1)$t
-# }, FUN.VALUE = 1)
-# durations.mean <- vector(length = length(results[[1]]))
-# for (i in 1:length(durations.mean)) {
-#   durations.mean[i] <- mean(vapply(results, function (run) {
-#     tail(run[[i]], 1)$t
-#   }, FUN.VALUE = 1))
-# }
-# configs <- c(1:steps)
-# e100k.durationsPerConfig <- data.frame(duration = durations.mean[c(31:40)], configs)
+plotAmount(subset(results.run0, size == 50000), 25, 5)
+
 plotTotalDuration(subset(results.durations, size == 100000))
 ggsave("mean-durations-100k.pdf", device = "pdf")
+plotTotalDuration(subset(results.durations, size == 10000)) +
+  scale_y_log10(name = "Duration (s)", breaks = seq(0, 5, 1))
+#plotTotalDuration(subset(results.durations, size == 200000))
+#ggsave("mean-durations-200k.pdf", device = "pdf")
