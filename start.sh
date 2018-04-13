44b20af609e0351cea77f4e04ab59a4533e4fb9e
Janis Krasemann <janiskra@mail.uni-paderborn.de>
2018-04-13 21:31:32 +0200

    Move client configuration to client folder

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker-compose up -d kibana elasticsearch eventstore evt-es-bridge app db web
-
+docker-compose -f docker-compose.yml -f client/docker-compose.yml up -d kibana elasticsearch eventstore evt-es-bridge app db web
